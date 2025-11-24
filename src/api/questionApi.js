f7e77ebdab73f8912d442f953b06de85945e891c
Ken <ken.nguyen160816@gmail.com>
2025-11-24 17:29:38 -0600

    updated for vercel

--- src/api/questionApi.js
+++ src/api/questionApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.boilerexams.com/questions/d44531f1-3cf7-404d-bd10-e9a786484b8a";
+const BASE_URL = "/api/questions/d44531f1-3cf7-404d-bd10-e9a786484b8a";
 
 /**
  * Returns the question data for a given question id.
